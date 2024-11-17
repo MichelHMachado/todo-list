060ed8e8ffb4dd1ed65704028953d8667c82fc9b
MichelHMachado <michelhmachado@gmail.com>
2024-11-17 03:24:55 -0300

    feat: setup vue routes

--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import VueRouter from " vue-router";
+import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import Signup from "@/views/Signup.vue";
@@ -7,9 +7,9 @@ import Signup from "@/views/Signup.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/login", name: "Login", component: Login },
-  { path: "/signup", name: "SignUp", component: Signup },
+  { path: "/", name: "HomePage", component: Home },
+  { path: "/login", name: "LoginPage", component: Login },
+  { path: "/signup", name: "SignUpPage", component: Signup },
 ];
 
 const router = new VueRouter({
