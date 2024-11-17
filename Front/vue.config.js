a0b612e1b0c231835d3fb9f6d85ec22595f9ee06
MichelHMachado <michelhmachado@gmail.com>
2024-11-17 03:24:14 -0300

    feat: setup frontend to run on https

--- Front/vue.config.js
+++ Front/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const fs = require("fs");
+const path = require("path");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  devServer: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs", "private-key.pem")),
+      cert: fs.readFileSync(
+        path.resolve(__dirname, "certs", "public-certificate.pem")
+      ),
+    },
+    port: 8080, // You can change the port to any available one
+    host: "localhost",
+    open: true, // This will automatically open the browser when you run `npm run serve`
+  },
+  transpileDependencies: true,
+});
