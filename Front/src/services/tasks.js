e2dcf9253946893c60c6f172449ac677ad1d6b5e
MichelHMachado <michelhmachado@gmail.com>
2024-11-17 23:51:27 -0300

    feat: add tasks services

---
+++ Front/src/services/tasks.js
@@ -0,0 +1,52 @@
+import { api } from "./api";
+
+export const createTask = async (taskData) => {
+  try {
+    const token = localStorage.getItem("access_token");
+    const response = await api.post("/tasks", taskData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchTasks = async () => {
+  try {
+    const response = await api.get("/tasks/");
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+  }
+};
+
+export const editTask = async (task) => {
+  try {
+    const response = await api.put(`/tasks/${task.uuid}`, task);
+
+    return response.data;
+  } catch (error) {
+    console.error("Error updating task:", error);
+  }
+};
+
+export const deleteTask = async (taskId) => {
+  try {
+    await api.delete(`/tasks/${taskId}`);
+  } catch (error) {
+    console.error("Error deleting task:", error);
+  }
+};
+
+export const toggleTaskCompleted = async (taskId) => {
+  try {
+    await api.patch(`/tasks/${taskId}/completed`);
+  } catch (error) {
+    console.error("Error toggling task completion:", error);
+  }
+};
