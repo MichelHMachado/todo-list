80b56b6242230d3aa5de4ff3d3503af7783b50b1
MichelHMachado <michelhmachado@gmail.com>
2024-11-17 23:47:36 -0300

    feat: add tasks routes

---
+++ backend/routes/tasks.js
@@ -0,0 +1,101 @@
+import express from "express";
+import Task from "../models/Task.js";
+import authenticate from "../middleware/authenticate.middleware.js";
+
+const router = express.Router();
+
+router.post("/", authenticate, async (req, res) => {
+  try {
+    const { title, description, priority } = req.body;
+    const { uuid: userUuid } = req.user;
+
+    const newTask = await Task.create({
+      title,
+      description,
+      priority,
+      userUuid,
+    });
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating task", error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.findAll();
+
+    res.status(201).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching tasks", error });
+  }
+});
+
+router.get("/:uuid", async (req, res) => {
+  try {
+    const task = await Task.findByPk(req.params.uuid);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching task", error });
+  }
+});
+
+router.put("/:uuid", async (req, res) => {
+  try {
+    const { title, description, priority, completed } = req.body;
+    const task = await Task.findByPk(req.params.uuid);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.priority = priority || task.priority;
+    task.completed = completed !== undefined ? completed : task.completed;
+
+    await task.save();
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating task", error });
+  }
+});
+
+router.delete("/:uuid", async (req, res) => {
+  try {
+    const task = await Task.findByPk(req.params.uuid);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    await task.destroy();
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting task", error });
+  }
+});
+
+router.patch("/:uuid/completed", async (req, res) => {
+  try {
+    const task = await Task.findByPk(req.params.uuid);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    task.completed = !task.completed;
+    await task.save();
+
+    res.status(200).json(task);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating task completion status", error });
+  }
+});
+
+export default router;
